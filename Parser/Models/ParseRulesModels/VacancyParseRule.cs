e63b52e50c51fbc2d9e6d0e6a4323318bf39a7ef
Artyom Sokolov <15012003artem@mail.ru>
2024-12-23 20:17:41 +0500

    fix VacancyParseRule

--- Parser/Models/ParseRulesModels/VacancyParseRule.cs
+++ Parser/Models/ParseRulesModels/VacancyParseRule.cs
@@ -11,35 +11,9 @@ public class VacancyParseRule
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
-
-    /// <summary>
-    /// Нода для получения названия компании
-    /// </summary>
-    public string CompanyNameNode { get; set; }
-
-    /// <summary>
-    /// Нода для получения названия вакансии
-    /// </summary>
-    public string NameNode { get; set; }
     
     /// <summary>
-    /// Правила для получения места работы
+    /// Правила парсинга страницы в формате json
     /// </summary>
-    public string CityNode { get; set; }
-
-    /// <summary>
-    /// Нодя для получения описания вакансии
-    /// </summary>
-    public string DescriptionNode { get; set; }
-    
-
-    /// <summary>
-    /// Нода для получения зарплаты
-    /// </summary>
-    public string SalaryNode { get; set; }
-    
-    /// <summary>
-    /// Нода для получения даты публикации вакансии
-    /// </summary>
-    public string CreationTimeNode { get; set; }
+    public String Rules { get; set; }
 }
